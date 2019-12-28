bd2daf3390edff199b2a942ee2dc577b034995f2
suwa3 <56011102+Ishizuka427@users.noreply.github.com>
2019-12-28 17:09:31 +0900

    Update test_calc.py

--- basic/calc/test_calc.py
+++ basic/calc/test_calc.py
@@ -6,4 +6,6 @@ import calc
     ["test_input", "expected"], [([10, 5], (15, 5)), ([3, 10], (13, -7)),],
 )
 def test_calc(test_input, expected):
-    assert calc.calc(*test_input) == expected
+        print(*test_input)
+        assert calc.calcadd(*test_input) == expected[0]
+        assert calc.calcsub(*test_input) == expected[1]
